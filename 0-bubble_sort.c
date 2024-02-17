8344d2b624f95437f0460a7736a8b731133cce71
lumasia07 <lumasiastancelaus@gmail.com>
2024-02-17 10:14:31 -0800

    bubble

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array in ascending order using bubble sort
+ * @array: Ponter to array of integers
+ * @size: size of array
+ * Return: Pointer to sorted array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int i, j;
+	size_t n, m;
+
+	for (n = 0; n < size - 1; n++)
+	{
+		j = 0;
+
+		for (m = 0; m < size - n - 1; m++)
+		{
+			if (array[m] > array[m + 1])
+			{
+				i = array[m];
+				array[m] = array[m + 1];
+				array[m + 1] = i;
+				j = 1;
+				print_array(array, size);
+			}
+		}
+		if (j == 0)
+			break;
+	}
+}
