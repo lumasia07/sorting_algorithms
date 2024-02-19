b0114601813e7c020ba03a3cc68a196202a916f7
lumasia07 <lumasiastancelaus@gmail.com>
2024-02-18 23:05:00 -0800

    selection

---
+++ 2-selection_sort.c
@@ -0,0 +1,9 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ * @array: array of integers
+ * @size: size of array
+ * Return: Always 0 (Success)
+ */
+void selection_sort(int *array, size_t size)
