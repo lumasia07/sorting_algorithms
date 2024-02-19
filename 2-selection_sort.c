9295f293b1f527e9925576e180a2f88d6d106dce
lumasia07 <lumasiastancelaus@gmail.com>
2024-02-18 23:41:42 -0800

    Select

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,3 +7,25 @@
  * Return: Always 0 (Success)
  */
 void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+	int tmp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+
+		if (min != i)
+		{
+			tmp = array[i];
+			array[i] = array[min];
+			array[min] = tmp;
+			print_array(array, size);
+		}
+	}
+}
